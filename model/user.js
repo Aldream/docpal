e685329c453d29fb555dded0b23e460eb57fa879
Aldream <benjamin.planche@aldream.net>
2013-10-01 16:44:55 +0200

    <Model> User Schema

--- model/user.js
+++ model/user.js
@@ -1,4 +1,30 @@
+/**
+ * =================
+ * SCHEMA - User
+ * 		by Benjamin (Bill) Planche / Aldream 
+ * =================
+ * Defines an user of the Jupiter System.
+ */
 var logger = require("../logger");
+
+var UserSchema = new Schema({
+	_id			: Schema.Types.ObjectId,
+	name  		: {type : String, default : 'Stranger', trim : true},
+	password  	: type : String, default : '', trim : false}
+})
+
+mongoose.model('User', UserSchema)
+
+//Ex:
+var vuHieu = new User({ name: 'Vu-Hieu', password: 'hop' });
+var bill = new User({ name: 'Bill', password: 'hop' });
+vuHieu.save(function (err, fluffy) {
+	// TODO handle the error
+});
+bill.save(function (err, fluffy) {
+	// TODO handle the error
+});
+
 
 var credentials = {
 	vuHieu: "hop",
@@ -11,6 +37,13 @@ var credentials = {
  */
 function getCredentials() {
 	logger.info("Gettings credentials...");
+	var credentials = {};
+	User.find(function (err, users) {
+		// TODO handle err
+		for (var u in users) {
+			credentials[u] = u.password;
+		}
+	});
 	return credentials;
 }
 
