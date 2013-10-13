9d2cf8dc8bd39b07d34c77608bb71959e52b75e8
Benjamin Planche <bill.murmure@gmail.com>
2013-10-13 21:25:54 +0200

    <Model> Corrections on Snapshot.

--- model/snapshot.js
+++ model/snapshot.js
@@ -24,8 +24,8 @@ module.exports = function(mongoose) {
 	 */
 	SnapshotSchema.statics.setPreOpCondition = function (func) {
 		SnapshotSchema.statics.preOpCondition = func;
-	})
-	SnapshotSchema.preOpCondition = function(op) {
+	}
+	SnapshotSchema.statics.preOpCondition = function(op) {
 		// Default condition: No snapshot before applying an operation.
 		return false;
 	}
@@ -40,12 +40,12 @@ module.exports = function(mongoose) {
 	 */
 	SnapshotSchema.statics.setPostOpCondition = function (func) {
 		SnapshotSchema.statics.postOpCondition = func;
-	})
-	SnapshotSchema.postOpCondition = function(op) {
+	}
+	SnapshotSchema.statics.postOpCondition = function(op) {
 		// Default condition: make a snapshot every 10 applied operations.
 		return !((++SnapshotSchema.statics.postOpCondition.counter)%10);
 	}
-	SnapshotSchema.statics.preOpCondition.counter = 0;
+	SnapshotSchema.statics.postOpCondition.counter = 0;
 
 	/**
 	 * makeSnapshotBefore
@@ -62,7 +62,7 @@ module.exports = function(mongoose) {
 				  }
 			});
 		}
-	})
+	}
 
 	/**
 	 * makeSnapshotAfter
@@ -79,7 +79,7 @@ module.exports = function(mongoose) {
 				  }
 			});
 		}
-	})
+	}
 
 	this.model = mongoose.model('Snapshot', SnapshotSchema);
 
